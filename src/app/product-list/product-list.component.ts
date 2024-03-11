e147c2076a75218f8a7aeccb93fb183f7c2ad185
MudassirAdmani <mudassiradmani466@gmail.com>
2024-03-12 02:10:18 +0500

    product list ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  standalone: true,
-  imports: [],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
-
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  errorMessage: string = '';
+  constructor(private productService: ProductService) { }
+  ngOnInit() {
+    this.productService.getProducts().subscribe(
+      (data) => {
+        this.products = data;
+      },
+      (error) => {
+        this.errorMessage = error.message;
+      }
+    );
+  }
 }
