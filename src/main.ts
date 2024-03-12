a8b74f2fc133d701f2a7730c4dc6140690206b54
MudassirAdmani <mudassiradmani466@gmail.com>
2024-03-13 01:15:40 +0500

    everything gonee

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { ProductListComponent } from './app/product-list/product-list.component';
-
-bootstrapApplication(ProductListComponent);
